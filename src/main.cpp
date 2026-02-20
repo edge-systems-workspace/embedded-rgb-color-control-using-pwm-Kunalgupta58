dd8f36aada711b00ee75eff3c95a19ba308395eb
Kunal Gupta <kunal.gupta_cs.aiml24@gla.ac.in>
2026-02-20 15:40:42 +0530

    Initialize Serial communication and configure RGB pins

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,19 @@
 
 void setup() {
 
-    // TODO 4:
-    // Initialize Serial communication (9600 baud)
+    
+    // TODO 4: Initialize Serial communication
+    Serial.begin(9600);
 
-    // TODO 5:
-    // Configure RGB pins as OUTPUT
+    // TODO 5: Configure RGB pins as OUTPUT
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
 
-    // TODO 6:
-    // Print initialization message
+    // TODO 6: Print initialization message
+    Serial.println("=================================");
+    Serial.println("     RGB LED Control System      ");
+    Serial.println("=================================");
 }
 
 void loop() {
